ae3b05a35c6c6a9bc849df8bac181a69c255b01b
James Cai <jcai1217@gmail.com>
2021-01-09 18:33:22 -0500

    fixed bugs

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# foo
-def main():
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
