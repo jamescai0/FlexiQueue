39952e59aca0a396428d69fbf4148a98e4556a1b
Amanda <amanda.ong44@outlook.com>
2021-01-09 22:43:00 -0700

    added login page

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def index():
 def test_link():
     return render_template('daldongane.html')
 
+from flask import redirect, url_for, request
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
