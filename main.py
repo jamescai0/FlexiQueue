85b59d6c1859c3ba558a7cefcfbf43bb6633382f
Alfred M <alfredmjz@gmail.com>
2021-01-10 12:37:53 +0800

    linking second webpage

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/daldongane")
+def test_link():
+    return render_template('daldongane.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
