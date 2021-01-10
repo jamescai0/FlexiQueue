1f80fa25f5cde2917121932f06d34657ac91ffa7
Alfred M <alfredmjz@gmail.com>
2021-01-10 15:00:13 +0800

    implemented real-date and time slots

--- static/index.js
+++ static/index.js
@@ -1,15 +1,40 @@
 window.onload = function () {
-    var curday = function (sp) {
-        today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1; //As January is 0.
-        var yyyy = today.getFullYear();
+    for (var i = 0; i < 7; i++) {
+        getDate(i);
+    }
 
-        if (dd < 10) dd = '0' + dd;
-        if (mm < 10) mm = '0' + mm;
-        return (dd + sp + mm + sp + yyyy);
-    };
+    var time = 12 + ".00" + "pm";
+    for (var i = 0; i <= 12; i++) {
+        var node = document.createElement("option");                 // Create a <option> node
+        if (i > 0) {
+            time = i + ".00" + "pm";
+        }
+        if (i == 12) {
+            time = i + ".00" + "am";
+        }
+        var textnode = document.createTextNode(time);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <option>
+        document.getElementById("time").appendChild(node);
+    }
 }
+
+function getTime() {
+    var now = new Date();
+    var time = now.getTime();
+}
+function getDate(current) {
+    var today = new Date();
+    var date = (today.getDate() + current) + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+    var node = document.createElement("option");                 // Create a <option> node
+    var textnode = document.createTextNode(date);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <option>
+    document.getElementById("date").appendChild(node);
+
+
+
+
+};
+
 
 window.onscroll = function () {
 
