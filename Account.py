dd0ea3f20c0671a49767c2fd2e5a5a1d5ff9784e
James Cai <jcai1217@gmail.com>
2021-01-09 23:41:21 -0500

    error

--- Account.py
+++ Account.py
@@ -1,17 +1,6 @@
 #creating login account
-def new_account:
-    username = raw_input("Create Username: ")
-    password = raw_input("Create Password: ")
-    store_u = []
-    store_p = []
 
-    if username in store_u:
-        print "Account already exists. Please try again."
-        return False
-    else:
-        store_u.append(username)
-        store_p.append(password)
-        return True
+
 
 
     
